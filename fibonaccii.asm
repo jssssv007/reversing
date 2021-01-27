ff7d4884b065be2131b88c989e7ae192891f99a9
jssssv007 <64346319+jssssv007@users.noreply.github.com>
2021-01-27 21:22:00 +0530

    Add files via upload

---
+++ fibonaccii.asm
@@ -0,0 +1,34 @@
+BITS 32
+extern scanf
+extern printf
+section .data
+
+
+	print:db "%d",0
+	x:db"fibonacci value is %d",10,0
+section .txt
+	global main
+	main:
+		push ebp
+		mov ebp,esp
+		sub esp,0x10
+		lea eax,[ebp-0x8]
+		push eax
+		push print
+		call scanf
+		mov eax,2
+		mov ebx,1
+		mov dword[ebp-0xc],0
+	solution:
+		mov dword[ebp-0x4],ebx
+		add ebx,dword[ebp-0xc]
+		mov ecx,dword[ebp-0x4]
+		mov dword[ebp-0xc],ecx
+		add eax,1
+		cmp eax,dword[ebp-0x8]
+		jle solution
+		push ebx
+		push x
+		call printf
+		leave
+		ret
